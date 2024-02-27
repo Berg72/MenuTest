53c6154597d84c56a9298caf9a9ebd14d6ac0c16
Berg72 <bergy450r@gmail.com>
2024-02-26 19:37:55 -0800

    updated

---
+++ Menu/Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Mark bergeson on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
